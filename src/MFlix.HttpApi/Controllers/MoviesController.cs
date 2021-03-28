3a0a60f2e93070930a9beb589e3f64dfdd9f1d3b
drminnaar <douglas.minnaar@gmail.com>
2021-03-28 23:36:09 +1300

    Remove commented-out code and cleanup movies controller

--- src/MFlix.HttpApi/Controllers/MoviesController.cs
+++ src/MFlix.HttpApi/Controllers/MoviesController.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Net;
 using System.Threading.Tasks;
-using FluentValidation.AspNetCore;
-using Grpc.Core;
 using MFlix.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -21,48 +18,6 @@ namespace MFlix.HttpApi.Controllers
             _movieService = movieService ?? throw new ArgumentNullException(nameof(movieService));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
-
-        //[HttpGet("{movieId}", Name = nameof(GetMovieById))]
-        //[ProducesResponseType(typeof(Movie), StatusCodes.Status200OK)]
-        //[ProducesResponseType(StatusCodes.Status404NotFound)]
-        //[ProducesResponseType(StatusCodes.Status406NotAcceptable)]
-        //[ProducesResponseType(StatusCodes.Status415UnsupportedMediaType)]
-        //[ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status422UnprocessableEntity)]
-        //[ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        //public async Task<IActionResult> GetMovieById([FromRoute] string movieId)
-        //{
-        //    if (string.IsNullOrEmpty(movieId))
-        //    {
-        //        ModelState.AddModelError(nameof(movieId), "A movie id is required");
-        //        return UnprocessableEntity(ModelState);
-        //    }
-
-        //    var request = new GetMovieByIdRequest { MovieId = movieId };
-
-        //    try
-        //    {
-        //        var response = await _movieService.GetMovieByIdAsync(request);
-
-        //        if (response?.Movie is null)
-        //            return NotFound();
-
-        //        return Ok(response.Movie);
-        //    }
-        //    catch (RpcException rpcException) when (rpcException.StatusCode == Grpc.Core.StatusCode.InvalidArgument)
-        //    {
-        //        rpcException.Trailers.ToValidationResult().AddToModelState(ModelState, prefix: null);
-        //        return ValidationProblem(statusCode: (int)HttpStatusCode.UnprocessableEntity, modelStateDictionary: ModelState);
-        //    }
-        //    catch (RpcException rpcException) when (rpcException.StatusCode == Grpc.Core.StatusCode.NotFound)
-        //    {
-        //        return NotFound();
-        //    }
-        //    catch (Exception exception)
-        //    {
-        //        _logger.LogError(exception, "A server fault occurred");
-        //        throw;
-        //    }
-        //}
 
         [HttpGet("{movieId}", Name = nameof(GetMovieById))]
         [ProducesResponseType(typeof(Movie), StatusCodes.Status200OK)]
