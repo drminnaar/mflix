dfa69164e32cd34f444fe2dbe6da8a97ab855a83
drminnaar <douglas.minnaar@gmail.com>
2021-04-02 14:56:57 +1300

    Add string extensions

---
+++ src/MFlix.FX/System/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace System
+{
+    public static class StringExtensions
+    {
+        public static IReadOnlyCollection<string> FromCsv(this string csv)
+        {
+            return csv
+                ?.Split
+                (
+                    new[] { "," },
+                    StringSplitOptions.RemoveEmptyEntries
+                )
+                ?.Select(_ => _.Trim())
+                ?.ToArray()
+                ?? Array.Empty<string>();
+        }
+    }
+}
