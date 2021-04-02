a7ac28c0a50be24c8f911c200e1a7ad4d6376bfd
drminnaar <douglas.minnaar@gmail.com>
2021-04-02 14:58:39 +1300

    Change grpc client host address

--- src/MFlix.HttpApi/Startup.cs
+++ src/MFlix.HttpApi/Startup.cs
@@ -22,7 +22,7 @@ namespace MFlix.HttpApi
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddGrpcClient<MovieService.MovieServiceClient>(options =>
             {
-                options.Address = new Uri("https://localhost:5001");
+                options.Address = new Uri("https://localhost:4001");
             });
             services
                 .AddControllers(options =>
